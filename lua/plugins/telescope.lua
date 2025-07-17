d0e7f8969d1c58c4ba812e1824e64875b97978c9
quangtran666 <quangprox121@gmail.com>
2025-07-17 22:12:08 +0700

    Setup treesitter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -37,7 +37,9 @@ return {
         require("telescope").load_extension("fzf")
 
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find files" })
-        vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = "Live grep" })
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+        vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+        vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Grep string" })
     end
 }
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ":TSUpdate",
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "tsx",
+            "typescript",
+            "xml",
+            "yaml",
+            "php",
+        },
+        auto_install = true,
+    }
+}
